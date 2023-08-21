549979dbdd3161d4467660e6a14dd739c02fbc66
muhammamdsurya <muhammadsurya2812@gmail.com>
2023-08-21 15:01:11 +0700

    hari pertama

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Junior Web Developer</title>
 </head>
+
 <body>
       <p>Kalimat ini dibuat menggunakan HTML saja</p>
       <?= "kalimat ini dibuat menggunakan PHP" ?>
 </body>
+
 </html>
+
+<!-- macam error 
+parse error -> sintak error
+fatal error -> mengakses fungsi tetapi fungsi belum di definisikan
+warning error -> mengakses file yang tidak ada
+notice error -> mengakses variable yang belum di definisikan 
+-->
