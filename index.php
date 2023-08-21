88219b9ceb2c2d40662a08b6c7c3e4b3033035ff
muhammamdsurya <muhammadsurya2812@gmail.com>
2023-08-21 13:44:04 +0700

    hari pertama

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Junior Web Developer</title>
+</head>
+<body>
+      <p>Kalimat ini dibuat menggunakan HTML saja</p>
+      <?= "kalimat ini dibuat menggunakan PHP" ?>
+</body>
+</html>
